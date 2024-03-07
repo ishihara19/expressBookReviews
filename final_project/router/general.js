c519946c018aefd03b9ec672ce35c0f80a7181f7
ishihara19 <stevenchicue@hotmail.com>
2024-03-07 11:13:15 -0500

    actualizacion

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,9 +50,10 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  
-  res.send(books[isbn])
-  return res.status(300).json({message: "Yet to be implemented"});
+  if(books[isbn]){
+    return res.status(200).send(books[isbn])
+  }
+    return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
@@ -61,7 +62,10 @@ public_users.get('/author/:author',function (req, res) {
     const author = req.params.author
     const booksArray = Object.values(books);
     let filter_author = booksArray.filter((books => books.author === author));
-    res.send(filter_author)
+    if(filter_author.length >0){
+        return res.status(200).send(filter_author)
+    }
+    
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
@@ -71,7 +75,10 @@ public_users.get('/title/:title',function (req, res) {
   const title = req.params.title
   const booksArray = Object.values(books);
   let filterTitle = booksArray.filter((books)=> books.title === title)
-  res.send(filterTitle)
+  if(filterTitle.length > 0){
+    return res.status(200).send(filterTitle)
+  }
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
