3ff6babc2b2e22a6e11b39722d189d212b543381
ishihara19 <stevenchicue@hotmail.com>
2024-03-10 13:30:10 -0500

    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,21 +29,42 @@ public_users.post("/register", (req,res) => {
   //return res.status(300).json({message: "Yet to be implemented"});
 });
 
+function getBookList(){
+   let myPromise = new Promise((resolve, reject) =>{
+      setTimeout(()=>{
+        resolve(books);
+      }, 10000)
+   })
+   return myPromise
+}
+
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
   //Write your code here
-  return res.status(200).send(JSON.stringify(books,null,4))
+  try {
+    const bookList = await getBookList();
+    return res.status(200).send(JSON.stringify(bookList,null,4))
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+  
   //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
-  const isbn = req.params.isbn;
-  if(!books[isbn]){
-    return res.status(400).json({message: "ibsn not exist"})
+  try {
+    const isbn = req.params.isbn;
+    if(!books[isbn]){
+      return res.status(400).json({message: "ibsn not exist"})
+    }
+    return res.status(200).send(books[isbn])
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
   }
-  return res.status(200).send(books[isbn])
+ 
 
  /* if(books[isbn]){
     return res.status(200).send(books[isbn])
@@ -52,8 +73,9 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
   //Write your code here
+  try {
     const author = req.params.author
     const booksArray = Object.values(books);
     let filter_author = booksArray.filter((books => books.author === author));
@@ -62,12 +84,19 @@ public_users.get('/author/:author',function (req, res) {
     }
     
   return res.status(404).json({message: `the Author ${author} not found`});
+    
+  } catch (error) {
+    
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+   
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
   //Write your code here
-  const title = req.params.title
+  try {
+    const title = req.params.title
   const booksArray = Object.values(books);
   let filterTitle = booksArray.filter((books)=> books.title === title)
   if(filterTitle.length > 0){
@@ -75,6 +104,11 @@ public_users.get('/title/:title',function (req, res) {
   }
   
   return res.status(404).json({message: `the title ${title} not found`});
+    
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+  
 });
 
 //  Get book review
